6b1bb2d5b0f4e7514399cb1ea910406fb89209b0
Bohong Huang <bohonghuang@qq.com>
2026-01-16 01:15:11 +0800

    Move `:around` methods of `expand-expr` into `override.lisp`

--- src/sugar/predef.lisp
+++ src/sugar/predef.lisp
@@ -1,24 +1,10 @@
 (in-package #:parsonic)
 
-(defmethod expand-expr :around ((op (eql 'satisfies)) &rest args)
-  (destructuring-bind (predicate) args
-    (if (equal predicate '(constantly nil))
-        (call-next-method)
-        (call-next-method
-         op (with-gensyms (result)
-              `(lambda (,result)
-                 (unless (eql ,result +input-eof+)
-                   (funcall ,predicate ,result))))))))
-
-(defmethod expand-expr :around ((op (eql 'rep)) &rest args)
-  (destructuring-bind (parser &optional (from 0) (to most-positive-fixnum)) args
-    (call-next-method op parser from to)))
-
 (defparser opt (parser)
   (or parser (constantly nil)))
 
 (defparser repsep (rep sep &optional (from 0) (to most-positive-fixnum))
-  (or (cons rep (rep (progn sep rep) (max (1- from) 0) (1- to))) (rep (or) from 1)))
+  (or (cons rep (rep (progn sep rep) (max (1- from) 0) (1- to))) (rep (or) from)))
 
 (defconstant eof +input-eof+)
 
